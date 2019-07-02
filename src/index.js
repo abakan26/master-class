24abf98c9b6a37bf97dc40a48476f1af448f22e2
Dmitriy <obakan.ac545@gmail.com>
2019-07-03 05:08:54 +0600

    error messages for validation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Inputmask from "inputmask";
 import * as EmailValidator from 'email-validator';
 
 window.onload = () => {
-EmailValidator.validate("asasldskl.sld")
+
 
 };
 
@@ -43,7 +43,7 @@ function valide_lenght(input, start, end){
 function onlyLetters(input){
     return /^[а-яА-Яa-zA-ZёЁ]*$/.test(input.value);
 }
-
+var btn_disabled = true;
 var name = document.getElementById("name");
 var count = document.getElementById("child_count")
 var email = document.getElementById("email");
@@ -58,19 +58,78 @@ var mask_day = new Inputmask("99");
 mask_day.mask(day);
 var mask_month = new Inputmask("99");
 mask_month.mask(month);
-day.oninput = (e) => { if (valide_inputmask(e.target)) month.focus()};
-month.oninput = (e) => { if (valide_inputmask(e.target)) year.focus()};
-name.oninput = () => document.getElementById("submit").disabled = validate();
-phone.oninput = () => document.getElementById("submit").disabled = validate();
-email.oninput = () => document.getElementById("submit").disabled = validate();
-count.oninput = () => document.getElementById("submit").disabled = validate();
-child_name.oninput = () => document.getElementById("submit").disabled = validate();
-child__second_name.oninput = () => document.getElementById("submit").disabled = validate();
+day.oninput = (e) => {
+    if (valide_inputmask(e.target)) {
+        month.focus()
+    }else {
+        btn_disabled = false
+    }
+};
+month.oninput = (e) => {
+    if (valide_inputmask(e.target)) year.focus()
+    else {
+        btn_disabled = false
+    }
+};
+month.oninput = (e) => {
+    if (!valide_inputmask(e.target)) {
+        btn_disabled = false
+    }
+};
+name.oninput = () => {
+    btn_disabled = validate();
+    if (!onlyLetters(name)){
+        show_error('name_error', true, 'имя должно содержать только буквы');
+        return true}else { show_error('name_error', false)}
+    if (!valide_lenght(name, 3, 20)){
+        show_error('name_error', true, "имя должно содержать не менее 3 букв");
+        return true} else { show_error('name_error', false)}
+};
+phone.oninput = () => {
+    btn_disabled = validate();
+
+    if (!valide_inputmask(phone)){
+        show_error('phone_error', true);
+        return true } else {show_error('phone_error', false);}
+};
+email.oninput = () => {
+    btn_disabled = validate();
+    if (!EmailValidator.validate(email.value)){
+        show_error('email_error', true);
+        return true } else {show_error('email_error', false);}
+};
+count.oninput = () => {
+    btn_disabled = validate();
+};
+child_name.oninput = () => {
+    btn_disabled = validate();
+    if (!onlyLetters(child_name)) {
+        show_error('child_secondname_error', true, 'фамилия должно содержать только буквы');
+        return true
+    } else {
+        show_error('child_secondname_error', false)
+    }
+    if (!valide_lenght(child_name, 3, 20)) {
+        show_error('child_secondname_error', true, "фамилия должно содержать не менее 3 букв");
+        return true
+    } else {
+        show_error('child_secondname_error', false)
+    }
+};
+child__second_name.oninput = () => {
+    btn_disabled = validate();
+    if (!onlyLetters(child__second_name)){
+        show_error('child_name_error', true, 'имя должно содержать только буквы');
+        return true}else { show_error('child_name_error', false)}
+    if (!valide_lenght(child__second_name, 3, 20)){
+        show_error('child_name_error', true, "имя должно содержать не менее 3 букв");
+        return true} else { show_error('child_name_error', false)}
+};
 
 function validate() {
-    if (!onlyLetters(name)){return true}
+    if (!onlyLetters(name)){ return true}
     if (!valide_lenght(name, 3, 20)){return true}
-    if (!valide_inputmask(phone)){return true}
+    if (!valide_inputmask(phone)){ return true }
     if (!EmailValidator.validate(email.value)){return true}
     if (!valide_lenght(count, 1, 10)){return true}
     if (!onlyLetters(child_name)){return true}
@@ -78,4 +137,23 @@ function validate() {
     if (!onlyLetters(child__second_name)){return true}
     if (!valide_lenght(child__second_name, 3, 20)){return true}
     return false
+};
+
+function show_error(element, bool, mess="проверьте введенные данные") {
+    if(bool){
+        var div = document.getElementById(element);
+        div.classList.add('show');
+        div.innerText = mess
+    } else {
+        document.getElementById(element).classList.remove('show')
+    }
+
+}
+
+
+document.getElementById("submit").onclick = (e)=>{
+    if(btn_disabled){
+        e.preventDefault()
+        show_error('submit_error', true, 'проверьте правильность введенных данных')
+    }else {show_error('submit_error', false)}
 };
